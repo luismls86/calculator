a2562ba2cb1ba27b2ab39548f3cdb6d60e5b4782
Luis <luismls86@gmail.com>
2022-01-04 20:44:57 +0100

    Set basic spring profiles with basic properties

--- src/main/java/com/iteriam/calculator/CalculatorApplication.java
+++ src/main/java/com/iteriam/calculator/CalculatorApplication.java
@@ -2,8 +2,10 @@ package com.iteriam.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
