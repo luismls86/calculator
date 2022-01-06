fc6adff293b29c450e73711fd8ad5b07c34bdea0
Luis <luismls86@gmail.com>
2022-01-06 17:55:02 +0100

    Trace result of operation

--- src/main/java/com/iteriam/calculator/service/implementation/BasicOperationServiceImpl.java
+++ src/main/java/com/iteriam/calculator/service/implementation/BasicOperationServiceImpl.java
@@ -5,6 +5,7 @@ import com.iteriam.calculator.common.exception.CustomException;
 import com.iteriam.calculator.service.dto.OperationDTO;
 import com.iteriam.calculator.common.enums.OperatorEnum;
 import com.iteriam.calculator.service.BasicOperationService;
+import io.corp.calculator.TracerImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -15,20 +16,30 @@ import java.math.BigDecimal;
 @Slf4j
 public class BasicOperationServiceImpl implements BasicOperationService {
 
-    ModelMapper mapper = new ModelMapper();
+    private ModelMapper mapper = new ModelMapper();
+    private TracerImpl tracer = new TracerImpl();
 
     @Override
     public OperationDTO operate(OperatorEnum operator, BigDecimal operandOne, BigDecimal operandTwo) throws CustomException {
 
         try {
+            String operationTrace = new StringBuilder()
+                    .append("Operation: ")
+                    .append(operandOne).append(" ")
+                    .append(operator).append(" ")
+                    .append(operandTwo)
+                    .toString();
+            log.info(operationTrace);
             Class<?> clazz = Class.forName(operator.getValue());
             Operation operation = (Operation) clazz.getDeclaredConstructor(BigDecimal.class, BigDecimal.class)
                     .newInstance(operandOne, operandTwo);
             operation.calculate();
             OperationDTO operationDTO = mapper.map(operation, OperationDTO.class);
+            tracer.trace(operationDTO);
             return operationDTO;
         } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | InvocationTargetException | NoSuchMethodException e) {
             log.error(e.getLocalizedMessage());
+            tracer.trace(e);
             throw new CustomException();
         }
     }
