7225bb5111351023fbef894a805db27ff2784c0f
Luis <luismls86@gmail.com>
2022-01-06 15:58:04 +0100

    Associate name of operation subclasses to operator enum to a dynamic instantiation of operation in the service

--- src/main/java/com/iteriam/calculator/common/enums/OperatorEnum.java
+++ src/main/java/com/iteriam/calculator/common/enums/OperatorEnum.java
@@ -1,6 +1,14 @@
 package com.iteriam.calculator.common.enums;
 
 public enum OperatorEnum {
-    add,
-    sub
+    add("com.iteriam.calculator.bean.Addition"),
+    sub("com.iteriam.calculator.bean.Subtraction");
+
+    private final String value;
+
+    OperatorEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() { return value; }
 }
