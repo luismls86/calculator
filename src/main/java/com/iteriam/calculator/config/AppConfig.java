5cdbd48c4583565f190fe7378e0b800ed2bf8eb6
Luis <luismls86@gmail.com>
2022-01-07 20:59:50 +0100

    Add Tracer as a bean

---
+++ src/main/java/com/iteriam/calculator/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.iteriam.calculator.config;
+
+import io.corp.calculator.TracerImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public TracerImpl createTracer() {
+        return new TracerImpl();
+    }
+}
